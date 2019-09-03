5977aba043ede98ec7c615298185beab1255d782
Nayan Das <dastms@gmail.com>
2019-09-04 00:23:39 +0530

    deploying to gh-pages 3

---
+++ precache-manifest.e058524406a1920adf0fe80cf3427329.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ba9c616b5cc8b6df94d3d4fe36c9d6",
+    "url": "/yawa/index.html"
+  },
+  {
+    "revision": "1724cf23864851d224a0",
+    "url": "/yawa/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "1c4b01686304c27f39ff",
+    "url": "/yawa/static/css/main.9977391b.chunk.css"
+  },
+  {
+    "revision": "1724cf23864851d224a0",
+    "url": "/yawa/static/js/2.7dcb0a31.chunk.js"
+  },
+  {
+    "revision": "1c4b01686304c27f39ff",
+    "url": "/yawa/static/js/main.fb99a8fe.chunk.js"
+  },
+  {
+    "revision": "9927fe1156e0c892d5d7",
+    "url": "/yawa/static/js/runtime~main.1c46f33d.js"
+  },
+  {
+    "revision": "8f7fd0571eabeb303fcc623e597fa6a0",
+    "url": "/yawa/static/media/night.8f7fd057.png"
+  }
+]);
